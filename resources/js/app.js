5967ad7cc8115d0a506e3a2bebda1715d49e3960
unknown <rifkymp0@gmail.com>
2025-01-14 08:09:12 +0700

    semua

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,27 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-import './bootstrap';
-import { createApp } from 'vue';
-import App from './Vue/App.vue';
-import router from './router';
 import '../css/app.css';
+import './bootstrap';
 
-createApp(App).use(router).mount('#app');
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue'),
+        ),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,11 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../components/Home.vue';
 import FormLaporan from '../components/FormLaporan.vue';
 import Riwayat from '../components/Riwayat.vue';
+import NotFound from '../components/NotFound.vue'; // Tambahkan file NotFound.vue
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/form-laporan', name: 'FormLaporan', component: FormLaporan },
   { path: '/riwayat', name: 'Riwayat', component: Riwayat },
+  // Rute untuk halaman tidak ditemukan
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
 ];
 
 const router = createRouter({
